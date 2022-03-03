70f3a7b75c46cfdec8a2395f5252ee4757674663
younminchan <dbsalscks0@naver.com>
2022-03-03 16:28:06 +0900

    update databinding

--- Databinding-kotlin/app/src/main/java/com/example/databinding_kotlin/MainActivity.kt
+++ Databinding-kotlin/app/src/main/java/com/example/databinding_kotlin/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.databinding_kotlin
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.databinding.DataBindingUtil
+import com.example.databinding_kotlin.databinding.ActivityMainBinding
 import com.example.databinding_kotlin.model.GitUserItem
 import com.example.databinding_kotlin.network.RetrofitService
 import retrofit2.Call
@@ -10,21 +12,32 @@ import retrofit2.Callback
 import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
+    var retrofit = RetrofitService.getInstance()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding: ActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.lifecycleOwner = this
+        binding
 
-        var retrofit = RetrofitService.getInstance()
+        initRetrofit()
+
+
+    }
+
+    fun initRetrofit(){
         retrofit.requestRegUser("younminchan").enqueue(object : Callback<ArrayList<GitUserItem>> {
             override fun onResponse(call: Call<ArrayList<GitUserItem>>, response: Response<ArrayList<GitUserItem>>) {
                 Log.e("YMC", "retrofit onResponse")
                 if(response.isSuccessful){
                     var res = response.body()
+
                     if (res != null) {
                         for(item in res){
                             Log.e("YMC", "name: ${item.name} / full_name: ${item.fullName}")
                         }
                     }
+
                 }
             }
 
---
+++ Databinding-kotlin/app/src/main/java/com/example/databinding_kotlin/viewmodel/MainViewModel.kt
@@ -0,0 +1,5 @@
+package com.example.databinding_kotlin.viewmodel
+
+class MainViewModel {
+
+}
