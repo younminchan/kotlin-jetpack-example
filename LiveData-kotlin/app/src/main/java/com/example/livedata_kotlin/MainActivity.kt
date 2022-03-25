ef85f2ab3e6e568a439cb9f9f7d9856971c034ad
younminchan <dbsalscks0@naver.com>
2022-03-25 13:31:23 +0900

    update LiveData

--- LiveData-kotlin/app/src/main/java/com/example/livedata_kotlin/MainActivity.kt
+++ LiveData-kotlin/app/src/main/java/com/example/livedata_kotlin/MainActivity.kt
@@ -2,10 +2,40 @@ package com.example.livedata_kotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
+import com.example.livedata_kotlin.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
+    private var liveText: MutableLiveData<String> = MutableLiveData()
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        liveDataInit()
+    }
+
+    private fun liveDataInit(){
+        binding.etInput.addTextChangedListener(object : TextWatcher{
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+                liveText.value = p0.toString()
+            }
+        })
+
+        liveText.observe(this, Observer {
+            binding.tvResult.text = "Livedata 값: $it"
+        })
     }
 }
